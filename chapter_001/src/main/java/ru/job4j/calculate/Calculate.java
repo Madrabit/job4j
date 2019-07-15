85a80fa216ba9d64008725b999fdfdd0dd68a939
madrabit <madrabot@gmail.com>
2019-07-15 14:07:38 +0300

    1.3. JavaDoc. Code convention[#159136]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+    /**
+    * Class Класс, который выводит  Хелоу.
+    * @author madrabit
+    * @since 15.07.2019
+    * @version 1
+    */
+
 public class Calculate {
+    /* 
+    * Main
+    * @params args - аргументы консоли.
+    */
     public static void main(String[] args) {
        System.out.println("Hello, Job4j.");
    }
