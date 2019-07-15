b650f7d159aa0d00903d3b1a811bc12a62e0e5cf
madrabit <madrabot@gmail.com>
2019-07-15 17:07:46 +0300

    2.5. Инспектор кода IDEA.[#159085]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -10,7 +10,6 @@ package ru.job4j.calculate;
 public class Calculate {
     /**
      * Main
-     * @params args - аргументы консоли.
      */
     public static void main(String[] args) {
         System.out.println("Hello, Job4j.");
