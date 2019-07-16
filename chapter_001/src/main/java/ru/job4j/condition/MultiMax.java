c08969c39cab2e523f43d28504f15e925ae7c29e
madrabit <madrabot@gmail.com>
2019-07-16 17:45:44 +0300

    4.4. Максимум из трех чисел[#159094]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+/**
+ * Класс показывающий максимум из 3-х чисел
+ * @author madrabit
+ * @version $Id$
+ * @since 1
+ */
+public class MultiMax {
+    /**
+     * Вычисление макисмума
+     * @param first первое число
+     * @param second второе число
+     * @param third третье число
+     * @return максимальное из трех
+     */
+    public int max(int first, int second, int third) {
+        int result = first >= second ? first : second;
+        return result >= third ? result : third;
+    }
+}
