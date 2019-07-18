875b4bd24db76c39bb850561050ba26ab38b679d
madrabit <madrabot@gmail.com>
2019-07-18 20:17:29 +0300

    5.5. Простые числа.[#159105]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+/**
+ * Простые числа
+ * @author madrabit
+ * @version $Id$
+ * @since 1
+ */
+public class PrimeNumber {
+    /**
+     * Метод вычисляет Простые числа (считается, число которое делится только на себя и на единицу).
+     * @param finish Число до которого нужно найти все "простые" числа
+     * @return возращает количество простых чисел
+     */
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            for (int j = 2; j <= i; j++) {
+                if ((i != j) && ((i % j) == 0)) {
+                    break;
+                } else if (i == j) {
+                    count++;
+                }
+
+            }
+        }
+        return count;
+    }
+}
