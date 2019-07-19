2fe4a4d3b35f846bed238ed37c5bd17e22d6653e
madrabit <madrabot@gmail.com>
2019-07-19 19:45:03 +0300

    6.3. Массив заполнен true или false[#159112]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * Массив заполнен true или false[#159112].
+ * @author madrabit on 19.07.19
+ * @version 1$
+ * @since 0.1
+ */
+public class Check {
+    /**
+     * Метод должен проверить, что все элементы в массиве являются true или false.
+     * @param data Проверяемый массив.
+     * @return Результат проеврки.
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        boolean flag = data[0];
+        for (int i = 1; i < data.length - 1; i++) {
+            if (flag != data[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author madrabit on 19.07.19
+ * @version 1$
+ * @since 0.1
+ */
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
