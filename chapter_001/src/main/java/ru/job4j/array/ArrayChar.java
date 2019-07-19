d27c3ac53528672609e46c337df1f5127e020c8b
madrabit <madrabot@gmail.com>
2019-07-19 19:53:45 +0300

    6.4. Слово начинается с ...[#159111]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * Слово начинается с ...[#159111]
+ * @author madrabit on 19.07.19
+ * @version 1$
+ * @since 0.1
+ */
+public class ArrayChar {
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean startsWith(String word, String prefix) {
+
+        char[] pref = prefix.toCharArray();
+        char[] wrd = word.toCharArray();
+
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != wrd[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author madrabit on 19.07.19
+ * @version $1
+ * @since 0.1
+ */
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar();
+        boolean result = word.startsWith("Hello", "He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar();
+        boolean result = word.startsWith("Hello", "Hi");
+        assertThat(result, is(false));
+    }
+}
