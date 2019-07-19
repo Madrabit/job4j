7dcc28c391cdeb4894330b1e6b3d76fd596dd896
madrabit <madrabot@gmail.com>
2019-07-19 20:00:11 +0300

    6.4.1. Слово заканчивается с ...[#159118]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * Слово заканчивается с ...[#159118]
+ * @author madrabit on 19.07.19
+ * @version 1$
+ * @since 0.1
+ */
+public class EndsWith {
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param post префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean endsWith(String word, String post) {
+
+        char[] pst = post.toCharArray();
+        char[] wrd = word.toCharArray();
+
+        for (int i = 0; i < pst.length; i++) {
+            if (pst[pst.length - (1 + i)] != wrd[wrd.length - (1 + i)]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
