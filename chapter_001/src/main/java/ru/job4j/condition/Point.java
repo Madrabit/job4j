4ad6480b41e01246cbe52fccd7087304cd94a8c3
madrabit <madrabot@gmail.com>
2019-07-25 15:07:02 +0300

    1. Рефакторинг - Расстояние между точками.[#159205]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -3,18 +3,34 @@ package ru.job4j.condition;
 /**
  * Класс вычисления расстояния между точками в системе координат.
  */
-
 public class Point {
+    /**
+     * X - точки
+     */
+    private int x;
+    /**
+     * Y - точки
+     */
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     /**
      * метод вычисления расстояния между точками.
-     * @param x1 x1
-     * @param y1 y1
-     * @param x2 x2
-     * @param y2 y2
+     * @param that точка.
      * @return расстояние
      */
-    public double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
+    }
+
+    /**
+     * Вывод координат точки
+     */
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 
@@ -13,18 +13,28 @@ import static org.junit.Assert.*;
 
 public class PointTest {
     @Test
-    public void whenPoints0020Then2() {
-        Point point = new Point();
-        double result = point.distance(0, 0, 2, 0);
-        double expected = 2D;
-        assertThat(result, closeTo(expected, 0.1));
+    public void whenZeroAndTenThenTen() {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 10);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(10D));
     }
 
     @Test
-    public void whenPoints1445Then3() {
-        Point point = new Point();
-        double result = point.distance(1, 4, 4, 5);
-        double expected = 3.1D;
-        assertThat(result, closeTo(expected, 0.1));
+    public void whenCheckItself() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
     }
 }
