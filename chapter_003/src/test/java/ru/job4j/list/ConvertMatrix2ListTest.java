095470c3b625c3b530922be51da0fb139f28897f
madrabit <madrabot@gmail.com>
2019-08-23 14:14:55 +0300

    слияние

---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.list;
+
+import list.ConvertMatrix2List;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author madrabit on 23.08.2019
+ * @version 1$
+ * @since 0.1
+ */
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
