520135652fa335aaae826df5b1645d93c395dab1
madrabit <madrabot@gmail.com>
2019-08-26 17:07:41 +0300

    сделал тест к Map

--- chapter_003/src/main/java/ru/job4j/list/User.java
+++ chapter_003/src/main/java/ru/job4j/list/User.java
@@ -6,6 +6,13 @@ package ru.job4j.list;
  * @since 0.1
  */
 public class User {
-    public Integer id;
-    public String name, city;
+    public final Integer id;
+    public final String name;
+    public final String city;
+
+    public User(Integer id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
 }
---
+++ chapter_003/src/test/java/ru/job4j/list/UserConvertTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.list;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author madrabit on 26.08.2019
+ * @version 1$
+ * @since 0.1
+ */
+public class UserConvertTest {
+    @Test
+    public void whenUserListThenHashMap() {
+        List<User> list = new ArrayList<>();
+        list.add(new User(1, "Вася", "Урюпинск"));
+        list.add(new User(2, "Коля", "Питер"));
+        UserConvert userConvert = new UserConvert();
+        HashMap map =  userConvert.process(list);
+        User item = (User) map.get(1);
+        String result = item.name;
+        Assert.assertThat(result, is("Вася"));
+    }
+}
