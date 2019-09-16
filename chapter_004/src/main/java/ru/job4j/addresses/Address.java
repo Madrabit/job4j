c40595226307a45db8519136d2cc785676356f4d
madrabit <madrabot@gmail.com>
2019-09-16 16:50:50 +0300

    3. Уникальность элементов и сортировка.[#159053]

--- chapter_004/src/main/java/ru/job4j/addresses/Address.java
+++ chapter_004/src/main/java/ru/job4j/addresses/Address.java
@@ -1,5 +1,7 @@
 package ru.job4j.addresses;
 
+import java.util.Objects;
+
 /**
  * Tourist's place of residence
  * @author madrabit on 16.09.2019
@@ -31,6 +33,10 @@ public class Address {
         this.apartment = apartment;
     }
 
+    public String getCity() {
+        return city;
+    }
+
     @Override
     public String toString() {
         return "Address{"
@@ -39,5 +45,25 @@ public class Address {
                 + ", home=" + home
                 + ", apartment=" + apartment
                 + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return home == address.home
+                && apartment == address.apartment
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
     }
 }
--- chapter_004/src/test/java/ru/job4j/addresses/MainTest.java
+++ chapter_004/src/test/java/ru/job4j/addresses/MainTest.java
@@ -15,14 +15,20 @@ import java.util.List;
 public class MainTest {
     @Test
     public void whenCollectThenReturnAddresses() {
-        Profile profile1 = new Profile(new Address("Moscow", "Lubyanka", 3, 33));
+        Profile profile1 = new Profile(new Address("Ural", "Lubyanka", 3, 33));
         Profile profile2 = new Profile(new Address("Spb", "Nevskiy", 5, 2));
+        Profile profile3 = new Profile(new Address("Spb", "Nevskiy", 5, 2));
+        Profile profile4 = new Profile(new Address("Moscow", "Nevskiy", 5, 2));
         List<Profile> list = new ArrayList<>();
         list.add(profile1);
         list.add(profile2);
+        list.add(profile3);
+        list.add(profile4);
         Main main =  new Main();
-        List<Address> addresses = main.collect(list);
-        assertThat("[Address{city='Moscow', street='Lubyanka', home=3, apartment=33}, Address{city='Spb', street='Nevskiy', home=5, apartment=2}]",
-                is(addresses.toString()));
+        List<Address> addresses = main.collectUnique(list);
+        assertThat(addresses.toString(),
+                is("[" + "Address{city='Moscow', street='Nevskiy', home=5, apartment=2}, "
+                        + "Address{city='Spb', street='Nevskiy', home=5, apartment=2}, "
+                        + "Address{city='Ural', street='Lubyanka', home=3, apartment=33}]"));
     }
 }
