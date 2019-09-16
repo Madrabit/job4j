0b8bcbf28e8fc1505fd5267be40c8a7b8b6080b3
madrabit <madrabot@gmail.com>
2019-09-16 16:56:02 +0300

    3. Уникальность элементов и сортировка.[#159053] + 1 тест

--- chapter_004/src/test/java/ru/job4j/addresses/MainTest.java
+++ chapter_004/src/test/java/ru/job4j/addresses/MainTest.java
@@ -15,6 +15,19 @@ import java.util.List;
 public class MainTest {
     @Test
     public void whenCollectThenReturnAddresses() {
+        Profile profile1 = new Profile(new Address("Spb", "Nevskiy", 5, 2));
+        Profile profile2 = new Profile(new Address("Moscow", "Nevskiy", 5, 2));
+        List<Profile> list = new ArrayList<>();
+        list.add(profile1);
+        list.add(profile2);
+        Main main =  new Main();
+        List<Address> addresses = main.collect(list);
+        assertThat(addresses.toString(),
+                is("[Address{city='Spb', street='Nevskiy', home=5, apartment=2}, " +
+                        "Address{city='Moscow', street='Nevskiy', home=5, apartment=2}]"));
+    }
+    @Test
+    public void whenCollectThenReturnSortedUniqueAddresses() {
         Profile profile1 = new Profile(new Address("Ural", "Lubyanka", 3, 33));
         Profile profile2 = new Profile(new Address("Spb", "Nevskiy", 5, 2));
         Profile profile3 = new Profile(new Address("Spb", "Nevskiy", 5, 2));
