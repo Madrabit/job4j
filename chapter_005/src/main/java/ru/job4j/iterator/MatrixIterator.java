8fe6eb3daca557debac538f6eca773c863aabbd1
madrabit <madrabot@gmail.com>
2019-10-01 16:04:05 +0300

    5.1.1. Итератор для двухмерного массива int[][][#185199]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,82 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+
+/**
+ * MatrixIterator
+ * @author madrabit on 01.10.2019
+ * @version 1$
+ * @since 0.1
+ */
+public class MatrixIterator implements Iterator {
+
+    private final int[][] values;
+    /**
+     * Index of first stage array
+     */
+    private int outer = 0;
+    /**
+     * Index of second stage array
+     */
+    private int index = 0;
+
+    public MatrixIterator(int[][] values) {
+        this.values = values;
+    }
+
+    /**
+     * Check first lvl array has next element or not
+     * @return array is finished or not
+     */
+    @Override
+    public boolean hasNext() {
+        if (values.length > outer) {
+            if (hasNextInside()) {
+                return true;
+            } else {
+                outer++;
+                if (values.length == outer) {
+                    return false;
+                }
+                return hasNextInside();
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check second lvl array has next element or not
+     * @return array is finished or not
+     */
+    private boolean hasNextInside() {
+        if (values[outer].length > index) {
+            return true;
+        } else {
+            this.index = 0;
+            return false;
+        }
+    }
+
+    /**
+     * Moves cursor forward
+     * @return Current value
+     */
+    @Override
+    public Object next() {
+        if (hasNext()) {
+            return innerNext();
+        } else {
+            outer++;
+            return innerNext();
+        }
+    }
+
+    /**
+     *  Moves cursor forward in second array
+     * @return Current value
+     */
+    private Object innerNext() {
+        return values[outer][index++];
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/iterator/MatrixIteratorTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author madrabit on 01.10.2019
+ * @version $
+ * @since 0.1
+ */
+public class MatrixIteratorTest {
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new MatrixIterator(new int[][]{{1}, {3, 4}, {7}});
+    }
+    @Test
+    public void hasNext() {
+        it.hasNext();
+        boolean result = it.hasNext();
+        assertThat(result, is(true));
+    }
+}
