211379fd647d1098f885eb382d5147aaa1e80b06
madrabit <madrabot@gmail.com>
2019-10-18 16:09:08 +0300

    1. Реализовать коллекцию Set на массиве[#185205]

--- chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -1,9 +1,57 @@
 package ru.job4j.set;
 
+import ru.job4j.generic.SimpleArray;
+
 /**
  * @author madrabit on 15.10.2019
  * @version 1$
  * @since 0.1
  */
-public class SimpleSet {
+public class SimpleSet<T> {
+    /**
+     * Container based on Simple Array List.
+     */
+    private final SimpleArray<T> container;
+
+    public SimpleSet(int size) {
+        container = new SimpleArray<>(size);
+    }
+
+    /**
+     * Add unique element to container.
+     *
+     * @param e Element for adding.
+     */
+    public void add(T e) {
+        if (checkHas(e)) {
+            container.add(e);
+        }
+    }
+
+    /**
+     * Check for an item.
+     *
+     * @param e Input item.
+     * @return Result isHas.
+     */
+    private boolean checkHas(T e) {
+        boolean result = false;
+        for (T t : container) {
+            if (t.equals(e)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Get element by index.
+     *
+     * @param index Input index.
+     * @return Returning element.
+     */
+    public T get(int index) {
+        return container.get(index);
+    }
 }
--- chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
+++ chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -1,9 +1,23 @@
 package ru.job4j.set;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * @author madrabit on 15.10.2019
  * @version 1$
  * @since 0.1
  */
 public class SimpleSetTest {
+    @Test
+    public void whenAdd121ThenLastElementNull() {
+        SimpleSet<Integer> set = new SimpleSet<>(3);
+        set.add(1);
+        set.add(2);
+        set.add(1);
+
+        assertThat(set.get(3), is(0));
+    }
 }
