b8ad2f4c5b3378d8920454099f0b1d413d659b00
madrabit <madrabot@gmail.com>
2020-02-12 16:34:55 +0300

    весь скрипт создания схемы заменил

--- chapter_007/db/scripts/update_001.sql
+++ chapter_007/db/scripts/update_001.sql
@@ -1,5 +1,6 @@
 create table task (
-   id serial primary key not null,
-   name varchar(2000),
+   id serial PRIMARY KEY,
+   task_name VARCHAR(50),
+   description VARCHAR(50),
    date TIMESTAMP without time zone
 );
