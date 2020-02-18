54d3cdeb876cadb0b8c7db3f144ad24abeb54f9b
madrabit <madrabot@gmail.com>
2020-02-18 10:47:08 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy.[#185150] - убрал вывод в лог в тестах

--- chapter_007/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_007/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,9 +1,8 @@
 package ru.job4j.tracker;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,9 +19,8 @@ import static org.junit.Assert.assertThat;
  */
 public class TrackerTest {
     private TrackerSQL sql;
-    private static final Logger LOG = LogManager.getLogger(TrackerSQL.class.getName());
 
-    public Connection init() {
+    public Connection init() throws IOException, ClassNotFoundException, SQLException {
         try (InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
             Properties config = new Properties();
             config.load(Objects.requireNonNull(in));
@@ -31,81 +29,65 @@ public class TrackerTest {
                     config.getProperty("url"),
                     config.getProperty("username"),
                     config.getProperty("password")
-
             );
-        } catch (Exception e) {
-            throw new IllegalStateException(e);
         }
     }
 
     @Test
-    public void createItem() {
+    public void createItem() throws SQLException, IOException, ClassNotFoundException {
         try (TrackerSQL sql = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             sql.add(new Item("name", "desc", System.currentTimeMillis()));
             assertThat(sql.findByName("name").size(), is(1));
-        } catch (SQLException e) {
-            LOG.error(e.getMessage(), e);
         }
-
     }
 
     @Test
-    public void findByIDReturnFirstItem() {
+    public void findByIDReturnFirstItem() throws SQLException, IOException, ClassNotFoundException {
         try (TrackerSQL sql = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item item = sql.add(new Item("name", "desc", System.currentTimeMillis()));
             Item newItem = sql.findById(item.getId());
             assertThat(newItem.getName(), is("name"));
-        } catch (SQLException e) {
-            LOG.error(e.getMessage(), e);
         }
     }
 
     @Test
-    public void findByNameReturnFirstTask() {
+    public void findByNameReturnFirstTask() throws SQLException, IOException, ClassNotFoundException {
         try (TrackerSQL sql = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             sql.add(new Item("1 task", "Some description", System.currentTimeMillis()));
             sql.add(new Item("2 task", "Any description", System.currentTimeMillis()));
             List<Item> items = sql.findByName("1 task");
             boolean result = items.stream().map(Item::getName).anyMatch(s -> Objects.equals(s, "1 task"));
             assertThat(result, is(true));
-        } catch (SQLException e) {
-            LOG.error(e.getMessage(), e);
         }
     }
 
 
     @Test
-    public void findAllReturnSize2() {
+    public void findAllReturnSize2() throws SQLException, IOException, ClassNotFoundException {
         try (TrackerSQL sql = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             sql.add(new Item("1 task", "Some description", System.currentTimeMillis()));
             sql.add(new Item("2 task", "Any description", System.currentTimeMillis()));
             List<Item> items = sql.findAll();
             assertThat(items.size(), is(2));
-        } catch (SQLException e) {
-            LOG.error(e.getMessage(), e);
         }
     }
 
     @Test
-    public void deleteReturnsTrue() {
+    public void deleteReturnsTrue() throws SQLException, IOException, ClassNotFoundException {
         try (TrackerSQL sql = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item item = sql.add(new Item("1 task", "Some description", System.currentTimeMillis()));
             boolean result = sql.delete(item.getId());
             assertThat(result, is(true));
-        } catch (SQLException e) {
-            LOG.error(e.getMessage(), e);
         }
     }
 
     @Test
-    public void replace() {
+    public void replace() throws SQLException, IOException, ClassNotFoundException {
         try (TrackerSQL sql = new TrackerSQL(ConnectionRollback.create(this.init()))) {
             Item item = sql.add(new Item("1 task", "Some description", System.currentTimeMillis()));
             Item newItem = new Item("Edited task", "Edited description", System.currentTimeMillis());
             boolean result = sql.replace(item.getId(), newItem);
             assertThat(result, is(true));
-        } catch (SQLException e) {
-            LOG.error(e.getMessage(), e);
         }
     }
 }
